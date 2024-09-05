dca3b346a82774d9e410c6f61f86446f6965dcd3
Egor Ostryakov <eg.ostr@mail.ru>
2024-09-05 20:49:39 +0300

    add more tests and parallel jobs in ci/cd

--- src/test/java/devops/app/MainTest.java
+++ src/test/java/devops/app/MainTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -16,15 +17,36 @@ public class MainTest {
     @Autowired
     private MockMvc mockMvc;
 
+    /*
+     Return auth view
+    */
     @Test
-    void userTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/user"))
-                .andExpect(status().is3xxRedirection());
+    void authTest() throws Exception {
+        String forwardedUrl = mockMvc.perform(MockMvcRequestBuilders.get("/auth/")).andExpect(status().isOk()).andReturn().getResponse().getForwardedUrl();
+        assertEquals("/index.html", forwardedUrl);
     }
 
+    /*
+     Redirects to auth page
+    */
+    @Test
+    void userTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user")).andExpect(status().is3xxRedirection());
+    }
+
+    /*
+     Redirects to auth page
+    */
     @Test
     void moderatorTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/moderator"))
-                .andExpect(status().is3xxRedirection());
+        mockMvc.perform(MockMvcRequestBuilders.get("/moderator")).andExpect(status().is3xxRedirection());
+    }
+
+    /*
+     Redirects to auth page
+    */
+    @Test
+    void wrongTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/3.14door")).andExpect(status().is3xxRedirection());
     }
 }
